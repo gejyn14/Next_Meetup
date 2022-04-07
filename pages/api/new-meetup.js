b3f24ce1870600e0690e6efdf41277b51cf564bc
Yu Jin An <anyj0516@g.skku.edu>
2022-04-08 03:05:42 +0900

    ready for deployment

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,28 @@
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv-safe";
+
+dotenv.config();
+
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
+
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    const client = await MongoClient.connect(
+      `mongodb+srv://gejyn14:EYU7EPIi0CIMlJAD@cluster0.bsucc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+    );
+
+    const db = client.db();
+
+    const meetupsCollection = await db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted!" });
+  }
+}
+
+export default handler;
